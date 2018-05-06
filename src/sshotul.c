fada8e1808fb616f284a60843102e9248ce012a9
zeroxthreef <zeroxthreef@gmail.com>
2018-05-06 17:10:36 -0500

    fixed the upload response

--- src/sshotul.c
+++ src/sshotul.c
@@ -602,7 +602,7 @@ int upload(struct http_request *req)
         kore_msg_send(KORE_MSG_WORKER_ALL, MESSAGE_ID, "update", strlen("update"));
 
 
-				asprintf(&redir, "%s/%lu%s", URL, (unsigned long)ID, strrchr(file->filename, '.'));
+				asprintf(&redir, "%s/%s%s", URL, IDstr, strrchr(file->filename, '.'));
 				kore_log(LOG_NOTICE, "File uploaded [%s] [%s]", file->filename, IDstr);
 				http_response_header(req, "Content-Type", "text/html"); /* for the page */
 				http_response_header(req, "Location", redir);
